a63e7bf000bab638284a77e154c09944f973f1c2
MichaelHornung1980 <103416199+MichaelHornung1980@users.noreply.github.com>
2022-05-23 13:03:46 +0200

    23.05.2022 erledigt

--- react3/src/Components/Counter.js
+++ react3/src/Components/Counter.js
@@ -2,22 +2,28 @@ import React from 'react';
 
 class Counter extends React.Component{
     constructor(props) {
-        super(props);
-        /*ToDO: Setze hier den initialen state von counter auf 0 */
-
+      super(props);
+      /*ToDO: Setze hier den initialen state von counter auf 0 */
+      this.state = {
+        counter: 0
+      }
     }
-
     increaseCounter = () => {
         /*ToDO: Erhöhe hier den aktuellen state von counter um +1 */
-
-        console.log("counter: ", this.state.counter);
+      this.setState ({counter: this.state.counter +1})
+      console.log("counter: ", this.state.counter);
     };
+
+
+
+
+
 
     render = () => {
         return (
             <div>
                 <button onClick={this.increaseCounter}>Like Counter</button>
-                <div>Peter hat: {this.state.counter} Likes</div>
+                <div>Michael hat: {this.state.counter} Likes</div>
             </div>
         );
     }
