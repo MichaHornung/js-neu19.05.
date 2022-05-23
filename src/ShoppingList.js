918ca45ac2e5f14861902f666a852ef084d4cf4c
MichaelHornung1980 <103416199+MichaelHornung1980@users.noreply.github.com>
2022-05-23 15:13:54 +0200

    23.05.2022 erledigt

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -38,6 +38,8 @@ class ShoppingList extends React.Component {
 
   erledigtAufZuKlappen() {
   // ToDo: fertig programmieren
+    let neuerZustand = !this.state.erledigtAufgeklappt
+    this.setState({erledigtAufgeklappt: neuerZustand})
   }
 
   render() {
@@ -54,6 +56,7 @@ class ShoppingList extends React.Component {
 
     let schonGekauft = []
     // ToDo: Bedingung  mit 'erledigtAufgeklappt' programmieren
+    if (this.state.erledigtAufgeklappt == true)
     for (const gruppe of App.gruppenListe) {
       schonGekauft.push(<GruppenTag
         key={gruppe.id}
@@ -93,8 +96,8 @@ class ShoppingList extends React.Component {
           <section>
             <h2>Schon gekauft
               {/* ToDo: füge hier drunter Deinen Code ein */}
-              <i className="material-icons">
-                expand_less
+              <i onClick={() => this.erledigtAufZuKlappen()} className="material-icons">
+                {this.state.erledigtAufgeklappt ? 'expand_more' : 'expand_less'}
               </i>
             </h2>
             <dl>
